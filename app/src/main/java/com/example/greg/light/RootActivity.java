8817cf7dc3a85e7c7ac77d5bfa82120852e85b6e
Greg Anderson <greg@infobytes.com>
2014-08-28 17:44:41 -0600

    Refactored code to just use one switch, also added some layout

--- app/src/main/java/com/example/greg/light/RootActivity.java
+++ app/src/main/java/com/example/greg/light/RootActivity.java
@@ -2,7 +2,9 @@ package com.example.greg.light;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
@@ -10,51 +12,31 @@ import android.widget.Switch;
 
 public class RootActivity extends Activity {
 
-    private Switch lightSwitch;
-    private Switch colorSwitch;
-    private ImageView light;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		LinearLayout rootLayout = new LinearLayout(this);
+		rootLayout.setOrientation(LinearLayout.VERTICAL);
+		setTheme(android.R.style.Theme_Black);
 
-        LinearLayout rootLayout = new LinearLayout(this);
-        rootLayout.setOrientation(LinearLayout.VERTICAL);
+		final ImageView light = new ImageView(this);
+		light.setImageResource(R.drawable.indicatorbw);
+		LinearLayout.LayoutParams lightParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 2);
+		rootLayout.addView(light, lightParams);
 
-        light = new ImageView(this);
-        light.setImageResource(R.drawable.indicatorbw);
-        rootLayout.addView(light);
+		final Switch lightSwitch = new Switch(this);
+		lightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+				light.setImageResource(b ? R.drawable.indicatorgreen : R.drawable.indicatorbw);
+			}
+		});
 
-        lightSwitch = new Switch(this);
-        lightSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (lightSwitch.isChecked())
-                    setLight();
-                else
-                    light.setImageResource(R.drawable.indicatorbw);
-            }
-        });
+		LinearLayout.LayoutParams lightSwitchParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 0, 1);
+		lightSwitchParams.gravity = Gravity.CENTER_HORIZONTAL;
+		rootLayout.addView(lightSwitch, lightSwitchParams);
 
-        colorSwitch = new Switch(this);
-        colorSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (lightSwitch.isChecked())
-                    setLight();
-            }
-        });
-
-        rootLayout.addView(lightSwitch);
-        rootLayout.addView(colorSwitch);
-
-        setContentView(rootLayout);
-    }
-
-    private void setLight() {
-        if (colorSwitch.isChecked())
-            light.setImageResource(R.drawable.indicatorgreen);
-        else
-            light.setImageResource(R.drawable.indicatorbright);
-    }
+		setContentView(rootLayout);
+	}
 }
